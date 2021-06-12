c83d105614b93bfa44d59aee7302ceb4e965af96
Geniy <gytraskovskiy@sberbank.ru>
2021-06-12 17:30:33 +0300

    client start

---
+++ start/client/src/index.tsx
@@ -0,0 +1,9 @@
+import {ApolloClient, NormalizedCacheObject} from '@apollo/client';
+import {cache} from './cache';
+
+const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    uri: 'http://localhost:4000/graphql',
+});
+
+
